2fcf5fb4c1128d98e807481741c5ab7fae693e57
KCH6937 <adasddasd12@naver.com>
2021-06-13 02:44:51 +0000

    BaekJoon10828(need explanation)

--- 10828-Stack/Exam10828.java
+++ 10828-Stack/Exam10828.java
@@ -1,18 +1,18 @@
 import java.util.*;
-//    https://deftkang.tistory.com/55 -> nextLine 에러 관련
+import java.lang.*;
+import java.io.*;
+
 class Exam10828 {   
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String str;
-        int isInData = 0;
-        String N = input.nextLine();
-        int K = Integer.parseInt(N);
-        
+        FastReader fr = new FastReader();
+        StringBuilder out = new StringBuilder();
         Stack<String> command = new Stack<>();
         
-        for(int i = 0; i < K; i++) {
-            
-            str = input.nextLine();
+        int N = fr.nextInt();
+        int isInData = 0;
+        
+        for(int i = 0; i < N; i++) {
+            String str = fr.nextLine();
             
             switch(str.substring(0,2)) {
                 case "pu" :
@@ -21,28 +21,61 @@ class Exam10828 {
                     break;
                 case "po" :
                     if(isInData > 0) {
-                        System.out.println(command.pop());
+                        out.append(command.pop() + "\n");
+                        isInData--;
                     } else {
-                        System.out.println("-1");
+                        out.append("-1\n");
                     }
                     break;
                 case "si" :
-                    System.out.println(command.size());
+                    out.append(command.size() + "\n");
                     break;
                 case "em" :
-                    if(command.empty() == true) {
-                        System.out.println(1);
+                    if(command.empty()) {
+                        out.append("1\n");
                     } else {
-                        System.out.println(0);
+                        out.append("0\n");
                     }
                     break;
                 case "to" :
-                    if(command.empty() == true) {
-                        System.out.println(-1);
+                    if(command.empty()) {
+                        out.append(-1 + "\n");
                     } else {
-                        System.out.println(command.peek());
+                        out.append(command.peek() + "\n");
                     }
             }
+        }
+        System.out.print(out);
+    }
+    
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer stk;
+        
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        
+        String next() {
+            while(stk == null || !stk.hasMoreElements()) {
+                try {
+                    stk = new StringTokenizer(br.readLine());
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }                            
+            }
+            return stk.nextToken();
+        }
+        
+        int nextInt() { return Integer.parseInt(next()); }
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch(IOException e) {
+                e.printStackTrace();                
+            }
+            return str;
         }
     }
 }
