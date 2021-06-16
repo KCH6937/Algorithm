5cf7db75d8a5dabf85d6e2b910b7bd065b5b1489
KCH6937 <adasddasd12@naver.com>
2021-06-16 13:48:13 +0000

    21/6/16 push(refer to devdynam0507's code)

--- 9012-PS/Exam9012.java
+++ 9012-PS/Exam9012.java
@@ -1,31 +1,43 @@
-import java.util.*;
 import java.io.*;
-
 
 public class Exam9012 {
     
-    // while문 필요?
+    private static final char parLeft = '(';
+    
+    public static String YesOrNo(String parenthesis) {
+        String result;
+        int judge = 0;
+        
+        char[] divParhs = parenthesis.toCharArray();
+        for(char o : divParhs) {
+            if(o == parLeft) {
+                judge++;
+            } else {                
+                judge--;
+            }
+            if(judge < 0) {
+                result = "NO";
+                break;
+            }
+        }
+        if(judge == 0) {
+            result = "YES";
+        } else {
+            result = "NO";
+        }
+        return result;
+    }
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        Stack<String> stack = new Stack<>();
-        StringBuilder out = new StringBuilder();
-        
-        int left = 0, right = 0;
-        char[] input;
-        
-        char parLeft = '(';
-        char parRight = ')';
-        
-        for(int i = 0; i < T; i++) {
-            
-            
-            
+        StringBuilder out = new StringBuilder();    
 
-                         
-                   
-            
+        String parenthesis;
+        for(int i = 0; i < T; i++) {
+            parenthesis = br.readLine();
+            out.append(YesOrNo(parenthesis) + "\n");
         }
+        System.out.println(out);
     }    
 }
