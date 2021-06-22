60b581adfd6ab013842be3a7a9574752c782aa09
KCH6937 <adasddasd12@naver.com>
2021-06-22 11:22:45 +0000

    error..

--- 1935-Postfix2/Exam1935.java
+++ 1935-Postfix2/Exam1935.java
@@ -1,12 +1,11 @@
 import java.io.*;
 import java.util.*;
-// 1748 start
+
 public class Exam1935 {
-    //private static final char[] operator = {'+', '-', '*', '/'};
     private static final int A = 65;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Stack<Integer> stack = new Stack<>();
+        Stack<Float> stack = new Stack<>();
         
         int N = Integer.parseInt(br.readLine());
         String input = br.readLine();
@@ -14,37 +13,41 @@ public class Exam1935 {
         
         char[] divStr = input.toCharArray();
         int[] value = new int[N];
-        float answer = 0;
+        int j = 0;
+        float popValue1 = 0, popValue2 = 0;
+        
         
         for(int i = 0; i < N; i++) {
             value[i] = Integer.parseInt(br.readLine());
         }
         
+        int temp = value[0];
+        
         for(int i = 0; i < size; i++) {
-            int judge = divStr[i];
-            int j = 0;
-            float popValue,popValue2;
-            if(judge >= A && judge < A + N) {
-                stack.push(value[j++]);
-            } else {                
-                popValue = stack.pop();
+            int judge = divStr[i];            
+            if(judge >= A && judge < A + N) {                
+                temp = temp == judge ? value[j] : value[j < (N-1) ? j++: j];
+                stack.push((float)temp);            
+            } else {            
+                popValue1 = stack.pop();
                 popValue2 = stack.pop();
-                stack.push(0);
+                
                 switch(divStr[i]) {
-                    case '+' :
-                        answer += popValue2 * popValue2;
+                    case '+' :                        
+                        stack.push(popValue2 + popValue1);
                         break;
                     case '-' :
-                        answer -= popValue2;
+                        stack.push(popValue2 - popValue1);
                         break;
                     case '*' :
-                        answer *= popValue2;
+                        stack.push(popValue2 * popValue1);
                         break;
                     case '/' :                        
-                        answer /= popValue2;
+                        stack.push(popValue2 / popValue1);
                 }                
             }
         }
-        System.out.format("%.2f \n", answer);
+        
+        System.out.format("%.2f", stack.pop());
     }
 }
